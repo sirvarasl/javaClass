79cbead329bfda9eeec566b8657cb46ccb5561e6
sirvarasl <47715629+sirvarasl@users.noreply.github.com>
2023-08-01 19:53:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+//Crea una variable llamada nombre y asígnale tu nombre como valor.
+ //Crea una variable llamada edad y asígnale tu edad como valor.
+// Crea una variable llamada mayorDeEdad y asígnale un valor booleano que indique si eres mayor de edad.
+// Crea una variable llamada direccion y asígnale un objeto con tu dirección.
+// Crea una variable llamada coloresFavoritos y asígnale un array con tus colores favoritos.
+// Crea una variable llamada lenguajesFavoritos y asígnale un array con tus lenguajes de programación favoritos.
+// Crea una variable llamada mejorLenguaje y asígnale el primer elemento del array lenguajesFavoritos.
+// Crea una variable llamada peorLenguaje y asígnale el último elemento del array lenguajesFavoritos.
+
+var nombre='sirvaras';
+
+
+
 console.log('hola mundo');
+
